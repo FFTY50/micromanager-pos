721dc92bbf50e9b33a06fa2e6b759150b8d15ef6
Wallace Reese <111623110+FFTY50@users.noreply.github.com>
2025-08-10 10:00:27 -0400

    Resolve welcome script merge conflict

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,4 +1,2 @@
 #!/usr/bin/env node
-
-console.log('Micromanager installed. Run scripts/dual-setup.sh to configure dual tty instances.');
-
+console.log('Micromanager installed. Run scripts/dual-setup.sh to configure dual instances.');
